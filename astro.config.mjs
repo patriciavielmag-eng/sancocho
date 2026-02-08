8284126f10a0045e1451f5dcd0a022367f1428e3
carlos <carlos@rigi.tech>
2026-02-08 01:07:05 +0100

    config: Add a trailing slash to the Astro base path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://patriciavielmag-eng.github.io',
-    base: '/sancocho',
+    base: '/sancocho/',
     integrations: [tailwind()],
 });
