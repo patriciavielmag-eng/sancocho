29614733ccbece481d938151b143b615e68cc8aa
carlos <carlos@rigi.tech>
2026-03-09 12:53:06 +0100

    feat: Add Lottie animation player, a new footer component, and a portrait image.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://patriciavielmag-eng.github.io',
     base: '/sancocho/',
-    integrations: [tailwind()],
+    integrations: [tailwind(), react()],
 });
